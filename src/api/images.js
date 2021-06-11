2f6e66accce1dabad84d3b3a430f92bb0714f030
Samir Shrestha <theonlysamir@gmail.com>
2021-06-11 23:28:03 +0545

    Validation for images added #1

--- src/api/images.js
+++ src/api/images.js
@@ -4,54 +4,114 @@ const { userAuthHandler } = require("../middlewares");
 const Image = require("../model/Image");
 const path = require("path");
 const { unlink } = require("fs");
+const Place = require("../model/Place");
+const { body, param, check, validationResult } = require("express-validator");
+const mongoose = require('mongoose')
+
+const isValidObjectId = param("id").custom((value) => {
+  const isValid = mongoose.Types.ObjectId.isValid(value);
+  if (!isValid) throw new Error("Invalid ID");
+  return isValid;
+});
+ 
+const placeExists = body("placeID").custom((value) => {
+  const isValid = mongoose.Types.ObjectId.isValid(value);
+  if (!isValid) throw new Error("Invalid ID");
+  return Place.findById(value).then((place) => {
+    if (!place) {
+      return Promise.reject("Place Doesnt Exist");
+    }
+    return true;
+  });
+});
 
 const photoUpload = upload.array("images", 5);
 
-router.post("/", userAuthHandler, photoUpload, async (req, res, next) => {
-  try {
-    const uploadedImages = req.files;
+const filesRequired = check("images")
+  .custom((value, { req }) => {
+    if (req.files.length > 0) return true;
 
-    const images = uploadedImages.map((img) => {
-      return {
-        user: req.user.id,
-        image: `/images/${img.filename}`,
-        place: req.body.placeID,
-      };
-    });
-    Image.insertMany(images);
-    res.json({ message: "images Saved" });
-  } catch (err) {
-    next(err);
-  }
-});
+    return false;
+  })
+  .withMessage("Images are required");
 
-router.delete("/:id", userAuthHandler, async (req, res, next) => {
-  try {
-    const image = await Image.findByIdAndDelete(req.params.id);
-    if (!image) return res.sendStatus(404);
-    if (image.user.toString() !== req.user.id) {
-      return res
-        .status(403)
-        .json({ message: "You aren't allowed to do that." });
+router.post(
+  "/",
+  userAuthHandler,
+  photoUpload,
+  placeExists,
+  filesRequired,
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const uploadedImages = req.files;
+
+      const images = uploadedImages.map((img) => {
+        return {
+          user: req.user.id,
+          image: `/images/${img.filename}`,
+          place: req.body.placeID,
+        };
+      });
+      Image.insertMany(images);
+      res.json({ message: "images Saved" });
+    } catch (err) {
+      next(err);
     }
-    unlink(path.join("static", image.image), (err) => {
-      if (err) return next(err);
-      return res.json({ message: "File Deleted" });
-    });
-  } catch (err) {
-    next(err);
   }
-});
+);
 
+router.delete(
+  "/:id",
+  isValidObjectId,
+  userAuthHandler,
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-router.post('/:id/flag',userAuthHandler,async (req,res,next)=>{
-  try{
-    const image = Image.findById(req.params.id);
-    image.flagged = true;
-    await image.save();
-    return res.json({message:"Image was Flagged"});
-  }catch(err){
-    next(err)
+      const image = await Image.findByIdAndDelete(req.params.id);
+      if (!image) return res.sendStatus(404);
+      if (image.user.toString() !== req.user.id) {
+        return res
+          .status(403)
+          .json({ message: "You aren't allowed to do that." });
+      }
+      unlink(path.join("static", image.image), (err) => {
+        if (err) return next(err);
+        return res.json({ message: "File Deleted" });
+      });
+    } catch (err) {
+      next(err);
+    }
   }
-})
+);
+
+router.post(
+  "/:id/flag",
+  isValidObjectId,
+  userAuthHandler,
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const image = await Image.findById(req.params.id);
+      if (!image) return res.sendStatus(404);
+      image.flagged = true;
+      await image.save();
+      return res.json({ message: "Image was Flagged" });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 module.exports = router;
